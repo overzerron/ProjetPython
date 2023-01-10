985ec456b690d82ba0493387d5115395449d8392
Jacky <pierlac.lacouve@gmail.com>
2023-01-10 23:20:22 +0100

    Added and called library.py in main.py

---
+++ library.py
@@ -0,0 +1,10 @@
+class Network:
+	address = "x.x.x.x"
+	mask = "x.x.x.x"
+	first_host = "x.x.x.x"
+	last_host = "x.x.x.x"
+	broadcast_host = "x.x.x.x"
+	max_host_count = 0
+	
+
+def display_network(network)
