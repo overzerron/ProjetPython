61611264231798d7c990f2f6822b8963331ba248
Chakhguir CHAKHGUEREEV <chakhguir.chakhguereev@etu.uca.fr>
2023-01-11 22:47:38 +0100

    Added Host number to div number conversion

--- main.py
+++ main.py
@@ -8,19 +8,24 @@ import library as l
 ## Code par Cha :
             
 def mainCha():
-    ipW,ipX,ipY,ipZ = l.askIp()
-    mskW,mskX,mskY,mskZ = l.askMask()
+    ipW,ipX,ipY,ipZ = 192, 168, 1, 0 #l.askIp()
+    mskW,mskX,mskY,mskZ = 255, 255, 255, 192 #l.askMask()
     ipBinary = l.ipToBinary(ipW,ipX,ipY,ipZ)
     mskBinary = l.ipToBinary(mskW,mskX,mskY,mskZ)
     if l.askTypeDiv() :
         somme = 32
-        nbHostMax = -2
+        nbAddressMax = 0
         for nb in (mskBinary):
             somme -= nb
-        for i in range(somme):
-            nbHostMax += 2**i
-        nbHost = l.askHostNb(nbHostMax)
-        
-        
+        nbAddressMax = 2**(somme+1)
+
+        nbHost = l.askHostNb(nbAddressMax-2)
+
+        i=0
+        while nbHost > 2**i-2:
+            i = i+1
+        nbDiv = int(nbAddressMax / 2**i)
+    else:
+        nbDiv = l.askNetNb()
 
 ###################################################################
