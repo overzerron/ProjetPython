b8d2a3c2899e39d9d9ffb056929350fdc6289f28
Chakhguir CHAKHGUEREEV <chakhguir.chakhguereev@etu.uca.fr>
2023-01-11 19:52:45 +0100

    Added mask calculation to mainCha

--- main.py
+++ main.py
@@ -6,7 +6,26 @@ import library as l
 ###################################################################
 ##############################  Cha  ##############################
 ## Code par Cha :
-
+subnet = 0
+nombreHotes = 0
+def mainCha():
+    ipW,ipX,ipY,ipZ = 192,168,1,1
+    mskW,mskX,mskY,mskZ = 255,255,255,0
+    choix = 1
+    nbBinaire = []
+    nbHotes = 2
+    if choix == 1 :
+        ###division par nb d'hotes     
+        resteDiv = masque % 2
+        nbBinaire.append(resteDiv)
+        masque = masque // 2
+        for i in range(3):
+        if resteDiv == 1 :
+            resteDiv = masque % 2
+            nbBinaire.append(resteDiv)
+            masque = masque // 2
+            if resteDiv = 0 :
+                return True
 
 
 ###################################################################
