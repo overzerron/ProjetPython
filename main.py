4cfb50e9aef7328f34ec63c8306361d4ea442388
Jacky <pierlac.lacouve@gmail.com>
2023-01-18 21:40:39 +0100

    FINISHED

--- main.py
+++ main.py
@@ -1,19 +1,11 @@
-############################## Jacky ##############################
-## Code par Jacky :
-
 import library as l
 
-###################################################################
-##############################  Cha  ##############################
-## Code par Cha :
-
-def mainCha():
+def main():
     ipW,ipX,ipY,ipZ = l.askIp()
     mskW,mskX,mskY,mskZ = l.askMask()
     ipBinary = l.ipToBinary(ipW,ipX,ipY,ipZ)
     mskBinary = l.ipToBinary(mskW,mskX,mskY,mskZ)
     somme = 32
-    nbAddressMax = 0
     for nb in (mskBinary):
         somme -= nb
     nbAddressMax = 2**(somme+1)
@@ -29,21 +21,38 @@ def mainCha():
         nbDiv = l.askNetNb(nbNetMax)
 
     ### Division du réseau ###
-    firstNullBitRank = getFirstNullBitRank(mskBinary)
+    firstNullBitRank = l.getFirstNullBitRank(mskBinary)
     divNumReal = 1
     startMskIp = firstNullBitRank
     while divNumReal < nbDiv:
         divNumReal *= 2
         mskBinary[startMskIp] = 1
         startMskIp += 1
-    resultMsk = l.binaryToIp(mskBinary)
+    resultMsk = "%d.%d.%d.%d"%(l.binaryToIp(mskBinary))
 
-    ipList = []
-    for i in range(firstNullBitRank, startMskIp+1):
+    addressList = []
+    for j in range(firstNullBitRank,32):
+        ipBinary[j]=0
+
+    somme = 32
+    for nb in (mskBinary):
+        somme -= nb
+    nbHostMax = 2**(somme+1)-2
+
+    for i in range(divNumReal):
         tmpIp = ipBinary
-        ipList.append(l.binaryToIp(tmpIp))
-        tmpIp[i]
+        j=firstNullBitRank
+        k=i+1
+        while i>0:
+            tmpIp[j]=i%2
+            j+=1
+            i=i//2
+        tmpNet = l.Network()
+        tmpNet.address = "%d.%d.%d.%d"%(l.binaryToIp(tmpIp))
+        tmpNet.mask = resultMsk
+        tmpNet.max_host_count = nbHostMax
+        l.displayNetwork(tmpNet,k)
 
 ###################################################################
 
-mainCha()
+main()
